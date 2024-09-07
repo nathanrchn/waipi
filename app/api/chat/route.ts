cc735469393703fb462fdb7cb148afbac42429d9
Nathan Ranchin <nathan.ranchin@epfl.ch>
2024-09-07 21:47:42 +0200

    removed unnecessary content

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,13 +1,17 @@
-import { openai } from "@ai-sdk/openai";
+import { createOpenAI } from "@ai-sdk/openai";
 import { convertToCoreMessages, streamText } from "ai";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
+  const aipi = createOpenAI({
+    baseURL: "https://aipi.vercel.app/api/",
+    apiKey: "aipi"
+});
+
   const result = await streamText({
-    model: openai("gpt-4o"),
-    system:
-      "do not respond on markdown or lists, keep your responses brief, you can ask the user to upload images or documents if it could help you understand the problem better",
+    model: aipi("openai:gpt-4o"),
+    system: "You are a helpful assistant.",
     messages: convertToCoreMessages(messages),
   });
 
